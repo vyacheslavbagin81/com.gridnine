62d9906938e41aa183df9f960cfbf001d47f0fb8
vyacheslavbagin81 <118987257+vyacheslavbagin81@users.noreply.github.com>
2024-07-22 13:12:33 +0300

    Исправил первый фильтр, чтобы проверка шла по всем сегментам

--- src/main/java/org/example/filter/ServiceFilter.java
+++ src/main/java/org/example/filter/ServiceFilter.java
@@ -2,6 +2,7 @@ package org.example.filter;
 
 import lombok.AllArgsConstructor;
 import org.example.model.Flight;
+import org.example.model.Segment;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,28 +13,39 @@ public class ServiceFilter {
 
     /**
      * Убираем вылет до текущего момента времени.
+     *
      * @param flightList = список перелетов
-     * @param time = время для условия
+     * @param time       = время для условия
      */
     public void flightsBeforeTheCurrentTime(List<Flight> flightList,
                                             LocalDateTime time) {
+        boolean check = true;
         for (Flight f : flightList) {
-            if (criterionSegment.flightsBeforeTheCurrentTime(f.getSegments(), time)) {
-                System.out.println("Перелет не соответствует условиям фильтра");
-            } else System.out.println(f);
+            for (Segment s : f.getSegments()) {
+                if (criterionSegment.flightsBeforeTheCurrentTime(f.getSegments(), time)) {
+//                    System.out.println("Перелет не соответствует условиям фильтра");
+                    check = false;
+                }
+            }
+            if (check) {
+                System.out.println(f);
+            }else System.out.println("Перелет не соответствует условиям фильтра");
+
+            check = true;
         }
     }
 
 
     /**
      * Убираем сегменты с датой прилёта раньше даты вылета.
+     *
      * @param flightList = список перелетов
      */
     public void arrivalDateBeforeDepartureDate(List<Flight> flightList) {
         for (Flight f : flightList) {
             if (criterionSegment.arrivalDateBeforeDepartureDate(f.getSegments())) {
                 System.out.println("Перелет не соответствует условиям фильтра");
-            }else System.out.println(f);
+            } else System.out.println(f);
         }
     }
 
@@ -41,15 +53,16 @@ public class ServiceFilter {
      * Перелеты, где общее время, проведённое на земле,
      * превышает два часа (время на земле — это интервал между прилётом одного сегмента
      * и вылетом следующего за ним).
+     *
      * @param flightList = список перелетов
-     * @param x = интересующий интервал времени
+     * @param x          = интересующий интервал времени
      */
-public void theTimeSpentOnEarthExceedsXHours(List<Flight> flightList, int x) {
-    for (Flight f : flightList) {
-        if (criterionSegment.theTimeSpentOnEarthExceedsXHours(f.getSegments(), x)) {
-            System.out.println("Перелет не соответствует условиям фильтра");
-        }else System.out.println(f);
+    public void theTimeSpentOnEarthExceedsXHours(List<Flight> flightList, int x) {
+        for (Flight f : flightList) {
+            if (criterionSegment.theTimeSpentOnEarthExceedsXHours(f.getSegments(), x)) {
+                System.out.println("Перелет не соответствует условиям фильтра");
+            } else System.out.println(f);
+        }
     }
-}
 
 }
